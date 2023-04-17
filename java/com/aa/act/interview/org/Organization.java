176aac7e8fac00b884dd321cbc5d106226ed7bc2
colton <colton@ForwardntoDawn.comp>
2023-04-16 19:15:16 -0700

    Implemented hire and methods needed to support it, added some documentation

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,37 +1,108 @@
 package com.aa.act.interview.org;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class Organization {
 
 	private Position root;
-	
+	// to produce unique employee IDs, starting at 1 in case the data ends up in a
+	// structure where is a flag or null value or similar.
+	protected AtomicInteger employeeIdGenerator = new AtomicInteger(1);
+
 	public Organization() {
 		root = createOrganization();
 	}
-	
+
 	protected abstract Position createOrganization();
-	
+
 	/**
 	 * hire the given person as an employee in the position that has that title
+	 * 
+	 * This implementation will also check if matching positions are filled or not
+	 * and will not overwrite existing employees.
 	 * 
 	 * @param person
 	 * @param title
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
+		List<Position> possibilities = findPosition(title);
+		if (possibilities.isEmpty()) {
+			return Optional.empty();
+		}
+
+		// Iterating through the results, attempting to put the candidate in the first
+		// unfilled position. This can result in some imprecision. One thing that might
+		// help with this was if the Position that the candidate would be a direct
+		// report of were included in the arguments.
+		for (Position toCheck : possibilities) {
+			if (!toCheck.isFilled()) {
+				toCheck.setEmployee(Optional.of(new Employee(employeeIdGenerator.getAndIncrement(), person)));
+				return Optional.of(toCheck);
+			}
+		}
 		return Optional.empty();
+	}
+
+	/**
+	 * It is possible that multiple instances of a title will exist (multiple VPs
+	 * might each have one "Middle Manager", or a VP Sales might have many
+	 * salespeople), so this will attempt to find all of them.
+	 * 
+	 * @param title
+	 * @return A list of all positions found that match the given title, empty list
+	 *         if none are found.
+	 */
+	protected List<Position> findPosition(String title) {
+		List<Position> result = new ArrayList<>();
+		findPositionHelper(title, root, result);
+		return result;
+	}
+
+	/**
+	 * Helper method to check nodes recursively
+	 * 
+	 * @param title
+	 * @param toCheck
+	 */
+	protected static void findPositionHelper(String title, Position toCheck, List<Position> result) {
+		// shouldn't happen, preparing anyway
+		if (toCheck == null || title == null) {
+			return;
+		}
+		// really shouldn't happen, but it's defined in this class, it should be handled
+		// in this class
+		if (result == null) {
+			throw new IllegalArgumentException("List to hold results should not be null");
+		}
+
+		if (toCheck.getTitle().equals(title)) {
+			result.add(toCheck);
+		}
+
+		toCheck.getDirectReports().forEach(position -> findPositionHelper(title, position, result));
 	}
 
 	@Override
 	public String toString() {
 		return printOrganization(root, "");
 	}
-	
+
+	/**
+	 * Recursively prints out the given position and every position listed as a
+	 * direct report or report-of-a-report, etc. Successively indents each level.
+	 * 
+	 * @param pos
+	 *            The position to use as a root node
+	 * @param prefix
+	 *            The string to prepend to listed entries
+	 */
 	private String printOrganization(Position pos, String prefix) {
 		StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
-		for(Position p : pos.getDirectReports()) {
+		for (Position p : pos.getDirectReports()) {
 			sb.append(printOrganization(p, prefix + "\t"));
 		}
 		return sb.toString();
--- java/com/aa/act/interview/org/Position.java
+++ java/com/aa/act/interview/org/Position.java
@@ -10,8 +10,21 @@ public class Position {
 
 	private String title;
 	private Optional<Employee> employee;
+
+	/*
+	 * If I understand correctly, Sets check whether an obect is already present
+	 * based on equals and hashCode. These methods are not overridden here, so Java
+	 * will use Object's equals and hashCode, which are based on memory addresses.
+	 * This theoretically allows someone to add many Positions with identical titles
+	 * as direct reports as long as they are newly-created obects and thus have
+	 * different memory addresses.
+	 * 
+	 * If this is not the intended behaviour, equals and hashCode should be
+	 * overridden, if it is the intended behaviour, perhaps this should be a list
+	 * rather than a set.
+	 */
 	private Set<Position> directReports;
-	
+
 	public Position(String title) {
 		this.title = title;
 		employee = Optional.empty();
@@ -20,36 +33,36 @@ public class Position {
 
 	public Position(String title, Employee employee) {
 		this(title);
-		if(employee != null)
+		if (employee != null)
 			setEmployee(Optional.of(employee));
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public void setEmployee(Optional<Employee> employee) {
 		this.employee = employee;
 	}
-	
+
 	public Optional<Employee> getEmployee() {
 		return employee;
 	}
-	
+
 	public boolean isFilled() {
 		return employee.isPresent();
 	}
-	
+
 	public boolean addDirectReport(Position position) {
-		if(position == null)
+		if (position == null)
 			throw new IllegalArgumentException("position cannot be null");
 		return directReports.add(position);
 	}
-	
+
 	public boolean removePosition(Position position) {
 		return directReports.remove(position);
 	}
-	
+
 	public Collection<Position> getDirectReports() {
 		return Collections.unmodifiableCollection(directReports);
 	}
